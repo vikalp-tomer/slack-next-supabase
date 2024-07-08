c07540e29d076d687f910051bdfa11aff5102b16
vikalp-tomer <vikalptomar15@gmail.com>
2024-07-08 14:43:12 +0530

    bootstraped shadcn project

---
+++ src/app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Lato } from "next/font/google";
+import "@/styles/globals.css";
+
+const lato = Lato({ subsets: ["latin"], weight: ["100", "300", "400", "700", "900"] });
+
+export const metadata: Metadata = {
+  title: "Slackzz",
+  description: "Slack clone by VikalpTomer",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={lato.className}>{children}</body>
+    </html>
+  );
+}
